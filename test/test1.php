b59e1133bdabddde1cff8671e663981084c10811
Mamadou WONE <mamadouwone12345@gmail.com>
2020-05-31 21:04:51 +0000

    Finish Riff Crash Course

---
+++ test/test1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="div">
+        <p>I learn JS</p>
+    </div>
+</body>
+</html>
+
+<script>
+    var d = document.getElementById('div');
+    alert(d);
+    var tabName = [];
+
+    // for (let index = 0; index < 5; index++) {
+    //     let name = prompt("Enter your name");
+    //     tabName.push(name);
+    // }
+    // tabName.forEach(element => {
+    //     alert(element);
+    // });
+
+    // dateDay = new Date();
+    // console.log(dateDay);
+
+</script>
