1f0f6afe730a954c37a1ada53a314e37fd4cd022
Mamadou WONE <mamadouwone12345@gmail.com>
2020-06-02 16:11:45 +0000

    16:00 understand DOM

--- Veille_JS/Match.php
+++ Veille_JS/Match.php
@@ -4,10 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title id="title">Document</title>
 </head>
 
 <body>
+    <div id="div">
+        <!-- <p id="boss">
+            WONE THE BOSS
+        </p> -->
+    </div>
+    <!-- <p>boss</p> -->
     <script>
         const psgMatch = [{
                 domicile: {
@@ -61,21 +67,53 @@
             }
         ]
 
-        const ulPere = document.createElement('ul');
+                // for (let Match of psgMatch) {
+                //     console.log(Match);
+                //     const matchLi = document.createElement('li');
+                //     document.body.appendChild(matchLi);
+                //     matchLi.innerHTML = `${Match.domicile.equipe}`;
+                // }
 
-        for (let match of psgMatch) {
-            const matchLi = document.createElement('li');
-            matchLi.innerText = `${match.domicile.equipe} vs ${match.exterieur.equipe}
-        ${match.domicile.score} - ${match.domicile.score}`;
+                // console.log(psgMatch);
+             
+                psgMatch.forEach(element => {
+                    console.log(element);
+                    const matchLi = document.createElement('p');
+                    document.body.appendChild(matchLi);
+                    matchLi.innerText = `${element.domicile.equipe} vs ${element.exterieur.equipe} ${element.domicile.score}-${element.exterieur.score}`;
+                   if (element.domicile.score > element.exterieur.score) {
+                    matchLi.innerHTML = `<strong>${element.domicile.equipe}</strong> vs ${element.exterieur.equipe} <strong>${element.domicile.score}</strong>-${element.exterieur.score}`;
+                   }else{
+                    matchLi.innerHTML = `${element.domicile.equipe} vs <strong>${element.exterieur.equipe}</strong> ${element.domicile.score}- <strong>${element.exterieur.score}</strong>`;
+                   }
+                });
 
-            if (match.domicile.score > match.exterieur.score) {
-                matchLi.innerHTML = `${match.domicile.equipe} vs ${match.exterieur.equipe}
-            <b>${match.domicile.score}-${match.exterieur.score}`;
-            } else {
-                matchLi.innerHTML = `${match.domicile.equipe} vs ${match.exterieur.equipe}
-            ${match.domicile.score}-<b>${match.exterieur.score}`;
-            }
-        }
+                    // titleConteneu = document.getElementById('title');
+                    // var query = document.querySelector('title');
+                    // var boss = document.getElementById('boss');
+                    // boss.style.color = 'blue';
+                    // boss.innerHTML = '<ul><li>This is an modification of tag value</li></ul>';
+                    // var createdAt = document.createElement('p');
+                    // var divParent = document.getElementById('div');
+                    // createdAt.innerHTML= 'New tag';
+                    // divParent.appendChild(createdAt);
+            
+                        // alert(query.innerText);
+        // const ulPere = document.createElement('ul');
+
+        // for (let match of psgMatch) {
+        //     const matchLi = document.createElement('li');
+        //     matchLi.innerText = `${match.domicile.equipe} vs ${match.exterieur.equipe}
+        // ${match.domicile.score} - ${match.domicile.score}`;
+
+        //     if (match.domicile.score > match.exterieur.score) {
+        //         matchLi.innerHTML = `${match.domicile.equipe} vs ${match.exterieur.equipe}
+        //     <b>${match.domicile.score}-${match.exterieur.score}`;
+        //     } else {
+        //         matchLi.innerHTML = `${match.domicile.equipe} vs ${match.exterieur.equipe}
+        //     ${match.domicile.score}-<b>${match.exterieur.score}`;
+        //     }
+        // }
     </script>
 </body>
 
