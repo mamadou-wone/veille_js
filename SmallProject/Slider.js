3cd18d9c320ab06661d6a657571eeae28dd8d72d
Mamadou WONE <mamadouwone12345@gmail.com>
2020-06-18 20:04:32 +0000

    Day4 #100DaysOfCode 4/100 Build a simple Slider Part 2

--- SmallProject/Slider.js
+++ SmallProject/Slider.js
@@ -1,50 +1,74 @@
 let picture = document.getElementById('slider');
-let spanContainer = document.getElementById('spanContainer');
-picture.style.backgroundColor = 'red';
-let slideNumber = 1;
-picture.innerHTML = slideNumber;
+// let spanContainer = document.getElementById('spanContainer');
+picture.style.backgroundColor = 'white';
+let slideNumber = 3;
+// picture.innerHTML = slideNumber;
+
+let image = new Image(250, 150);
+image.src = "boss.jpg";
+picture.appendChild(image);
 
 let nextClick = document.getElementById('next');
+// nextClickValue = picture.innerText;
+
 let previousClick = document.getElementById('prev');
+// previousClickValue = picture.innerText;
 
-function Previous() {
-    previousSlider = picture.innerText;
-    sliderValue = slideNumber--;
-    switch (sliderValue) {
-        case 1:
-            picture.innerHTML = 2;
-            break;
-        case 2:
-            picture.innerHTML = 3;
-            break;
-        case 3:
-            picture.innerHTML = 4;
-            break;
-    }
 
+
+let nextClickInit = 1;
+// let nextClickValue = nextClickInit++;
+let tabSlider = [];
+for (let nextClickValue = 0; nextClickValue < 4; nextClickValue++) {
+    tabSlider = nextClickValue;
+    console.log(tabSlider);
 }
 
-function Next() {
-    sliderValue = slideNumber++;
-    switch (sliderValue) {
-        case 1:
-            picture.innerHTML = 2;
-            break;
-        case 2:
-            picture.innerHTML = 3;
-            break;
-        case 3:
-            picture.innerHTML = 4;
-            break;
-    }
+function sliderClick() {
 
+    if (nextClickValue <= slideNumber) {
+        switch (nextClickValue) {
+            case 1:
+                image.src = "boss2.jpg";
+                picture.appendChild(image);
+                break;
+            case 2:
+                image.src = "boss3.jpg";
+                picture.appendChild(image);
+                break;
+            case 3:
+                image.src = "boss4.jpg";
+                picture.appendChild(image);
+                break;
+        }
+        console.log(nextClickValue);
+    }
+}
+
+function sliderPreviousClick() {
+    if (nextClickValue === slideNumber) {
+        nextClickValue--;
+        switch (nextClickValue) {
+            case 2:
+                image.src = "boss2.jpg";
+                picture.appendChild(image);
+                break;
+            case 1:
+                image.src = "boss3.jpg";
+                picture.appendChild(image);
+                break;
+        }
+        console.log(nextClickValue);
+
+    }
 }
 
 nextClick.addEventListener('click', function click() {
-    Next();
-    // console.log(sliderValue);
+    sliderClick();
 });
 
+
+
 previousClick.addEventListener('click', function click() {
-    Previous();
+    sliderPreviousClick();
 })
