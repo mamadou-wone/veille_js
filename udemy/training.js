900f96660da6b32eb78866919a23505ff295ed3a
Mamadou WONE <mamadouwone12345@gmail.com>
2020-06-28 15:54:35 +0000

    Day 1 with js  udemy crash course

---
+++ udemy/training.js
@@ -0,0 +1,12 @@
+// function multi(number) {
+//     for (let i = 0; i <= 10; i++) {
+//         console.log(`${number}` + "*" + `${i}` + "=" + number * i);
+
+//     }
+// }
+
+function sayHi(name) {
+    alert('Hello ' + name);
+}
+
+sayHi("udemy");
