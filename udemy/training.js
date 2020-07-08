782c415285dadfd801126f398de2fdefa66af7b4
Mamadou WONE <mamadouwone12345@gmail.com>
2020-07-08 14:58:56 +0000

    Day10 js #100DaysOfCode [10/100] Le Juste Prix

--- udemy/training.js
+++ udemy/training.js
@@ -312,41 +312,111 @@
 // *****************************GENERATEUR DE CITATION********************************
 
 // Tableau de citations
-let citations = [
-    ["La vie est un mystère qu'il faut vivre, et non un problème à résoudre.", "Gandhi"],
-    ["Le plus grand risque est de ne prendre aucun risque.", "Mark Zuckerberg"],
-    ["Méritez votre statut de leader chaque jour.", "Mickael Jordan"],
-    ["Soyez le changement que vous voulez voir dans le monde.", "Gandhi"],
-    ["A chaque fois que vous vous retrouvez du même côté que la majorité, il est temps de prendre du recul, et de réfléchir.", "Mark Twain"],
-    ["Seulement ceux qui prendront le risque d’aller trop loin découvriront jusqu’où on peut aller.", "T.S Elliot"],
-    ["Le succès c’est tomber sept fois, se relever huit.", "Proverbe japonais"],
-    ["Dans vingt ans vous serez plus déçus par les choses que vous n’avez pas faites que par celles que vous avez faites. Alors sortez des sentiers battus. Mettez les voiles. Explorez. Rêvez. Découvrez.", "Mark Twain"],
-    ["Si vous attendez pour agir, tout ce que vous gagnerez, avec le temps, c’est de l’âge.", "Brian Tracy"],
-    ["Quand on concentre son attention sur un seul projet, l’esprit suggère constamment des idées et des améliorations qui lui échapperaient s’il était occupé avec plusieurs projets en même temps.", "P.T. Barnum"],
-    ["Se dédier à faire tout ce que l’on peut pour aider les autres à obtenir ce qu’ils veulent, c’est la clé du succès.", "Brian Sher"],
-    ["Si vous pensez que vous êtes trop petit pour avoir de l’impact, essayez d’aller au lit avec un moustique.", "Anita Roddick"],
-    ["Ne jugez pas chaque jour sur ce que vous récoltez, mais sur les graines que vous semez.", "Robert Louis Stevenson"],
-    ["L’action est la clé fondamentale de tout succès.", "Pablo Picasso"],
-    ["Le succès, c’est se promener d’échecs en échecs tout en restant motivé.", "Winston Churchill"],
-    ["Votre avenir est créé par ce que vous faîtes aujourd’hui, pas demain.", "Robert T. Kiyosaki"],
-    ["Ne vous découragez pas, c’est souvent la dernière clef du trousseau qui ouvre la porte.", "Zig Ziglar"],
-    ["Pour gagner votre vie, apprenez à l’école. Pour gagner une fortune, apprenez par vous-même.", "Brian Tracy"],
-    ["Les gagnants trouvent des moyens, les perdants des excuses…", "F. D. Roosevelt"],
-    ["Vous n’êtes jamais trop vieux pour vous fixer de nouveaux buts, ou rendre vos rêves réalité.", "C.S. Lewis"],
-    ["Un pessimiste voit la difficulté dans chaque opportunité. Un optimiste voit une opportunité dans chaque difficulté.", "Winston Churchill"]
-];
+// let citations = [
+//     ["La vie est un mystère qu'il faut vivre, et non un problème à résoudre.", "Gandhi"],
+//     ["Le plus grand risque est de ne prendre aucun risque.", "Mark Zuckerberg"],
+//     ["Méritez votre statut de leader chaque jour.", "Mickael Jordan"],
+//     ["Soyez le changement que vous voulez voir dans le monde.", "Gandhi"],
+//     ["A chaque fois que vous vous retrouvez du même côté que la majorité, il est temps de prendre du recul, et de réfléchir.", "Mark Twain"],
+//     ["Seulement ceux qui prendront le risque d’aller trop loin découvriront jusqu’où on peut aller.", "T.S Elliot"],
+//     ["Le succès c’est tomber sept fois, se relever huit.", "Proverbe japonais"],
+//     ["Dans vingt ans vous serez plus déçus par les choses que vous n’avez pas faites que par celles que vous avez faites. Alors sortez des sentiers battus. Mettez les voiles. Explorez. Rêvez. Découvrez.", "Mark Twain"],
+//     ["Si vous attendez pour agir, tout ce que vous gagnerez, avec le temps, c’est de l’âge.", "Brian Tracy"],
+//     ["Quand on concentre son attention sur un seul projet, l’esprit suggère constamment des idées et des améliorations qui lui échapperaient s’il était occupé avec plusieurs projets en même temps.", "P.T. Barnum"],
+//     ["Se dédier à faire tout ce que l’on peut pour aider les autres à obtenir ce qu’ils veulent, c’est la clé du succès.", "Brian Sher"],
+//     ["Si vous pensez que vous êtes trop petit pour avoir de l’impact, essayez d’aller au lit avec un moustique.", "Anita Roddick"],
+//     ["Ne jugez pas chaque jour sur ce que vous récoltez, mais sur les graines que vous semez.", "Robert Louis Stevenson"],
+//     ["L’action est la clé fondamentale de tout succès.", "Pablo Picasso"],
+//     ["Le succès, c’est se promener d’échecs en échecs tout en restant motivé.", "Winston Churchill"],
+//     ["Votre avenir est créé par ce que vous faîtes aujourd’hui, pas demain.", "Robert T. Kiyosaki"],
+//     ["Ne vous découragez pas, c’est souvent la dernière clef du trousseau qui ouvre la porte.", "Zig Ziglar"],
+//     ["Pour gagner votre vie, apprenez à l’école. Pour gagner une fortune, apprenez par vous-même.", "Brian Tracy"],
+//     ["Les gagnants trouvent des moyens, les perdants des excuses…", "F. D. Roosevelt"],
+//     ["Vous n’êtes jamais trop vieux pour vous fixer de nouveaux buts, ou rendre vos rêves réalité.", "C.S. Lewis"],
+//     ["Un pessimiste voit la difficulté dans chaque opportunité. Un optimiste voit une opportunité dans chaque difficulté.", "Winston Churchill"]
+// ];
+
+// function getRand(rand) {
+//     return Math.floor(Math.random() * Math.floor(rand));
+// }
+
+// let citaionValue = document.getElementById('citation');
+// let author = document.getElementById('auteur');
+// let newCitaion = document.getElementById('nouveau');
+// let citationIndex;
+
+// newCitaion.addEventListener('click', () => {
+//     citationIndex = getRand(21);
+//     citaionValue.innerHTML = `${citations[citationIndex][0]}`;
+//     author.innerHTML = `${citations[citationIndex][1]}`;
+// });
+
+let input = document.getElementById('prix');
+let formulaire = document.getElementById('formulaire');
+let error = document.querySelector('small');
+
+// error.innerHTML = null;
+error.style.display = "none";
+
+// Génération d'un nombre aléatoire
 
 function getRand(rand) {
-    return Math.floor(Math.random() * Math.floor(rand));
+    return Math.floor((Math.random() * Math.floor(rand)));
 }
 
-let citaionValue = document.getElementById('citation');
-let author = document.getElementById('auteur');
-let newCitaion = document.getElementById('nouveau');
-let citationIndex;
 
-newCitaion.addEventListener('click', () => {
-    citationIndex = getRand(21);
-    citaionValue.innerHTML = `${citations[citationIndex][0]}`;
-    author.innerHTML = `${citations[citationIndex][1]}`;
+let nombreAleatoire = getRand(1001);
+let nombreChoisi;
+let coup = 0;
+let instructions = document.getElementById('instructions');
+
+
+function verifier(number) {
+    let instruction = document.createElement('div');
+    if (number < nombreAleatoire) {
+        instruction.innerText = 'Plus Grand';
+        instruction.className = 'plus';
+        // instructions.append(instruction);
+        console.log('Plus grand');
+    } else if (number > nombreAleatoire) {
+        console.log('Plus petit');
+        instruction.innerText = 'Plus Petit';
+        instruction.className = 'moins';
+        // instructions.append(instruction);
+
+    } else {
+        instruction.innerText = 'Bravo Félicitation :)';
+        instruction.className = 'fini';
+        input.disabled = true;
+        // input.style = 'deseabled';
+        console.log('Bravo');
+    }
+    instructions.prepend(instruction);
+}
+
+// console.log(nombreAleatoire);
+
+// Vérifiez si le user donne bien un nombre
+input.addEventListener('keyup', () => {
+    if (isNaN(input.value)) {
+        error.style.display = "contents";
+    } else {
+        error.style.display = "none";
+    }
+
 });
+
+// Traitement du formulaire
+
+formulaire.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (isNaN(input.value) || input.value == '') {
+        input.style.borderColor = "red";
+    } else {
+        input.style.borderColor = "silver";
+        coup++;
+        nombreChoisi = input.value;
+        input.value = '';
+        verifier(nombreChoisi);
+    }
+})
